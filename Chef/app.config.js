259af5515d54c90a87d3fb1c62a99d0978e6d90c
qfareapp <me.pranjal@gmail.com>
2026-04-04 15:38:15 +0530

    Use dynamic Expo config for Chef Firebase file

---
+++ Chef/app.config.js
@@ -0,0 +1,32 @@
+const googleServicesFile = process.env.GOOGLE_SERVICES_JSON || './google-services.json';
+
+module.exports = {
+  expo: {
+    name: 'NeighbourBites Chef',
+    slug: 'foodsood-chef',
+    version: '1.0.0',
+    orientation: 'portrait',
+    userInterfaceStyle: 'light',
+    splash: {
+      resizeMode: 'contain',
+      backgroundColor: '#2D9B6F',
+    },
+    ios: {
+      supportsTablet: false,
+    },
+    android: {
+      googleServicesFile,
+      adaptiveIcon: {
+        backgroundColor: '#2D9B6F',
+      },
+      predictiveBackGestureEnabled: false,
+      package: 'com.mepranjal.foodsoodchef',
+    },
+    web: {},
+    extra: {
+      eas: {
+        projectId: '0831a9f2-5277-4065-a175-b4a949fd1009',
+      },
+    },
+  },
+};
